c3dcc2596cb2797f7065a8513ae6197a122f694c
monosniper <vanteateam@gmail.com>
2022-11-27 22:53:33 +0500

    sad

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import Sequelize from 'sequelize';
 import dotenv from 'dotenv'
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
-
+console.log('HOST: ', process.env.DATABASE_HOST)
 const sequelize = new Sequelize(
     process.env.DATABASE,
     process.env.DATABASE_USER,
